b223734a78fec59dfdd294209079324608b3d876
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-07-05 16:32:27 +0300

    5.1.2. Создать итератор четные числа [#313770]

--- chapter_001/src/main/java/it/EvenIterator.java
+++ chapter_001/src/main/java/it/EvenIterator.java
@@ -13,7 +13,16 @@ public class EvenIterator implements Iterator<Integer> {
 
     @Override
     public boolean hasNext() {
-        return (nextEvenPossition(data, point) >= 0) ?  true : false;
+        boolean res = false;
+        for (int i = point; i < data.length; i++) {
+            if (point < data.length && data[i] % 2 == 0) {
+                res = true;
+                break;
+            } else {
+                point++;
+            }
+        }
+        return res;
     }
 
     @Override
@@ -21,24 +30,6 @@ public class EvenIterator implements Iterator<Integer> {
         if (!hasNext()) {
             throw new NoSuchElementException();
         }
-        return data[nextEvenPossition(data, point)];
-    }
-
-    public int nextEvenPossition (int[] data, int point) {
-        int possition = 0;
-        if (point == 0) {
-            possition = point;
-        } else {
-            possition = point + 1;
-        }
-        for (int i = possition; i < data.length; i++) {
-            if (possition < data.length && data[i] % 2 == 0) {
-                possition = i;
-                break;
-            } else {
-                possition = -1;
-            }
-        }
-        return possition;
+        return data[point++];
     }
 }
