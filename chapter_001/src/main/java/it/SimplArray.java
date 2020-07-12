bcff6e6300fd1a328f720499ea19e878a03ce72e
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-07-12 13:00:20 +0300

    5.2.1. Реализовать SimpleArray<T>[156#313784]

--- chapter_001/src/main/java/it/SimplArray.java
+++
@@ -1,8 +0,0 @@
-package it;
-
-public interface SimplArray<T> extends Iterable<T> {
-    boolean add(T model);
-    T set(int index, T model);
-    void remove(int index);
-    T get(int index);
-}
--- chapter_001/src/main/java/it/SimpleArray.java
+++ chapter_001/src/main/java/it/SimpleArray.java
@@ -1,74 +1,57 @@
 package it;
 
-import java.util.Arrays;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.Objects;
 
-public class SimpleArray<T> implements SimplArray<T> {
-    private Object[] data;
+public class SimpleArray<T> implements Iterable<T> {   private Object[] data = new Object[200];
     private int possitions = 0;
 
-    public SimpleArray(int size) {
-        this.data = new Object[size];
+    public void add(T model) {
+        data[possitions++] = model;
     }
 
-    @Override
-    public boolean add(T model) {
-        try {
-            if (possitions == data.length) {
-                data = Arrays.copyOf(data, 2 * possitions);
-            }
-            data[possitions++] = model;
-            return true;
-        } catch (ClassCastException ex) {
-            ex.printStackTrace();
-        }
-        return false;
-    }
-
-    @Override
-    public void T set (int index, T model) {
+    public void set(int index, T model) {
         Objects.checkIndex(index, possitions);
         data[index] = model;
     }
 
-    @Override
     public T remove(int index) {
         Objects.checkIndex(index, possitions);
-        T value  = (T) data[index];
+        T value = (T) data[index];
         System.arraycopy(data, index + 1, data, index, data.length - index - 1);
         data[data.length - 1] = null;
         possitions--;
         return value;
     }
 
-    @Override
     public T get(int index) {
         return (T) data[index];
     }
 
     @Override
     public Iterator<T> iterator() {
-        class SimpleArrayIterator<T> implements Iterator<T> {
-            private int index = 0;
-            private Object[] values;
-            public SimpleArrayIterator(Object[] values) {
-                this.values = values;
-            }
+        return new Iterator<T>() {
+            private int index;
+
             @Override
             public boolean hasNext() {
-                return index < values.length;
+                return index < possitions;
             }
+
             @Override
             public T next() {
-                return (T) values[index++];
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return (T) data[index++];
             }
-        }
-        return new SimpleArrayIterator<T>(data);
+        };
     }
 
+
     public static void main(String[] args) {
-        SimpleArray<String> strings = new SimpleArray<>(2);
+        SimpleArray<String> strings = new SimpleArray<>();
         strings.add("BMW");
         strings.add("AUDY");
         strings.add("MERCEDESS");
