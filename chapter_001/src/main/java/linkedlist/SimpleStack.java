6c883e96661d99a4b56e550eb8633e89fb43f71f
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-07-15 12:31:49 +0300

    4. Используя контейнер на базе связанного списка создать контейнер Stack[71474#313762]

--- chapter_001/src/main/java/linkedlist/SimpleStack.java
+++ chapter_001/src/main/java/linkedlist/SimpleStack.java
@@ -3,9 +3,8 @@ package linkedlist;
 public class SimpleStack<E> {
     private ForwardLinked<E> linked = new ForwardLinked<E>();
     public E pop() {
-        E resalt = linked.get(linked.size() - 1);
-        linked.deleteLast();
-        return resalt;
+        linked.get(linked.size() - 1);
+        return linked.deleteLast();
     }
 
     public void push(E value) {
