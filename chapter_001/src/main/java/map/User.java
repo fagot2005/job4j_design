b19f4e65ff4f36d542b15f370f3f3309e19f00b1
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-07-16 23:11:30 +0300

    2. Без переопределения equals и hashCode [#313795]

--- chapter_001/src/main/java/map/User.java
+++ chapter_001/src/main/java/map/User.java
@@ -1,6 +1,9 @@
 package map;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class User {
     public String name;
@@ -11,5 +14,14 @@ public class User {
         this.name = name;
         this.children = children;
         this.birdhday = birdhday;
+    }
+
+    public static void main(String[] args) {
+        User us1 = new User("Ivan", 2, new GregorianCalendar(1970, 12, 5));
+        User us2 = new User("Ivan", 2, new GregorianCalendar(1970, 12, 5));
+        Map<User, Object> map = new HashMap<>();
+        map.put(us1, "USER1");
+        map.put(us2, "USER2");
+        System.out.println(map);
     }
 }
