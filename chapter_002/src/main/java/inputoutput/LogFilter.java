d93684f9df21dbca33d7b32cfa247f6c3843f474
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-07-23 23:29:28 +0300

    0.4. BufferedOutputStream[252490#313813]

--- chapter_002/src/main/java/inputoutput/LogFilter.java
+++ chapter_002/src/main/java/inputoutput/LogFilter.java
@@ -1,10 +1,8 @@
 package inputoutput;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class LogFilter {
     public static List<String> filter(String file) {
@@ -20,10 +18,22 @@ public class LogFilter {
 
     public static void main(String[] args) {
         List<String> log = filter("log.txt");
+        save(log, "404.txt");
         for (String l:log
         ) {
             System.out.println(l);
         }
-        //System.out.println(log);
+    }
+
+    private static void save(List<String> log, String file) {
+        try (PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(file)))) {
+            for (String str:log
+            ) {
+                out.write(str + "\r\n");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("File " + file + " not assecc for writing");
+        }
     }
 }
