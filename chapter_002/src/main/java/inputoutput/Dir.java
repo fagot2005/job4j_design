0af529089dedfbc26fc11aeb7f028fcdaddf3f94
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-07-28 18:45:11 +0300

    5. Валидация параметров запуска. [#313810]

--- chapter_002/src/main/java/inputoutput/Dir.java
+++ chapter_002/src/main/java/inputoutput/Dir.java
@@ -4,19 +4,21 @@ import java.io.File;
 
 public class Dir {
     public static void main(String[] args) {
-        File file = new File("E:\\Java\\Java_Project\\job4j_design");
-if (!file.exists()) {
-    throw new IllegalArgumentException(String.format("No exists %s", file.getAbsoluteFile()));
-    }
-if (!file.isDirectory()) {
-    throw new IllegalArgumentException(String.format("No directory %s", file.getAbsoluteFile()));
-}
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
+        }
+        File file = new File(args[0]);
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("No exists %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("No directory %s", file.getAbsoluteFile()));
+        }
         System.out.println(String.format("size : %s", file.getTotalSpace()));
-for (File subfile : file.listFiles()) {
-    if (!subfile.isDirectory()) {
-        System.out.println("File name " + subfile.getName() + ", size file " + subfile.length() + " b");
-    }
-
-}
+        for (File subfile : file.listFiles()) {
+            if (!subfile.isDirectory()) {
+                System.out.println("File name " + subfile.getName() + ", size file " + subfile.length() + " b");
+            }
+        }
     }
 }
