769a5fca47adacec941a5a539f5f650601f9612c
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-07-30 14:49:01 +0300

    4.1. Сканирование файловой системы. [#313808]

--- chapter_002/src/main/java/inputoutput/Search.java
+++ chapter_002/src/main/java/inputoutput/Search.java
@@ -5,19 +5,20 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        if (args.length == 0) {
+        if (args.length != 2) {
             throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
         }
-        Path start = Paths.get(args[0]);
-        search(start, args[1]).forEach(System.out::println);
+        Path start = Paths.get("E:\\Java\\Java_Project\\job4j_design");
+        search(start, p -> p.toFile().getName().endsWith(".java")).forEach(System.out::println);
     }
 
-    public static List<Path> search(Path root, String ext) {
-        SearchFiles searcher = new SearchFiles(p -> p.toFile().getName.endsWith(ext));
+    public static List<Path> search(Path root, Predicate<Path> ext) throws IOException {
+        SearchFiles searcher = new SearchFiles(ext);
         Files.walkFileTree(root, searcher);
-        return searcher.getPaths();;
+        return searcher.getPaths();
     }
 }
