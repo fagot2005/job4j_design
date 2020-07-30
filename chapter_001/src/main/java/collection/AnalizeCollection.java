9462afe42138e23a6ffc4f82d944bc44054d7b3f
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-07-30 22:00:43 +0300

    2. Статистику по коллекции. [#313757]

--- chapter_001/src/main/java/collection/AnalizeCollection.java
+++ chapter_001/src/main/java/collection/AnalizeCollection.java
@@ -1,6 +1,5 @@
 package collection;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,45 +8,31 @@ public class AnalizeCollection {
 
     public int[] diff(List<UserColl> previous, List<UserColl> current) {
         int[] info = new int[3];
-        List<Integer> cheng = new ArrayList<>();
-        List<Integer> tempAdd = new ArrayList<>();
-        List<Integer> tempDell = new ArrayList<>();
         Map<Integer, UserColl> previousMap = new HashMap<>();
-        Map<Integer, UserColl> currentMap = new HashMap<>();
         for (int i = 0; i < previous.size(); i++) {
             previousMap.put(previous.get(i).id, previous.get(i));
         }
-        for (int i = 0; i < current.size(); i++) {
-            currentMap.put(current.get(i).id, current.get(i));
-        }
         for (Integer prevMap : previousMap.keySet()) {
-            for (Integer currMap : currentMap.keySet()) {
-                if (previousMap.get(prevMap).id == currentMap.get(currMap).id) {
-                    if (!previousMap.get(prevMap).name.equals(currentMap.get(currMap).name)) {
-                        cheng.add(prevMap);
-                        tempAdd.add(currMap);
+            for (UserColl curr : current) {
+                if (curr.id == previousMap.get(prevMap).id) {
+                    if (!previousMap.get(prevMap).name.equals(curr.name)) {
+                        info[1]++;
+                        info[0]++;
                     } else {
-                        tempAdd.add(currMap);
+                        info[0]++;
                     }
                 }
             }
         }
-        for (Integer currMap : currentMap.keySet()) {
+        for (UserColl curr : current) {
             for (Integer prevMap : previousMap.keySet()) {
-                if (currentMap.get(currMap).id == previousMap.get(prevMap).id) {
-                    tempDell.add(prevMap);
+                if (curr.id == previousMap.get(prevMap).id) {
+                    info[2]++;
                 }
             }
         }
-        for (int i = 0; i < tempAdd.size(); i++) {
-            currentMap.remove(tempAdd.get(i));
-        }
-        for (int i = 0; i < tempDell.size(); i++) {
-            previousMap.remove(tempDell.get(i));
-        }
-        info[0] = currentMap.size();
-        info[1] = cheng.size();
-        info[2] = previousMap.size();
+        info[0] = current.size() - info[0];
+        info[2] = previousMap.size() - info[2];
         return info;
     }
 
@@ -58,14 +43,6 @@ public class AnalizeCollection {
         public UserColl(int id, String name) {
             this.id = id;
             this.name = name;
-        }
-
-        public int getId() {
-            return id;
-        }
-
-        public String getName() {
-            return name;
         }
     }
 }
