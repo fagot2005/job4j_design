70ee9c2feaf2b2b21f00b2ca383c0814d811a451
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-08-02 12:07:52 +0300

    6. Кодировка. [#313819]

--- chapter_002/src/main/java/bot/ConsolBot.java
+++ chapter_002/src/main/java/bot/ConsolBot.java
@@ -1,35 +1,54 @@
 package bot;
 
-import java.io.*;
-import java.util.Random;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class ConsolBot {
     public static void main(String[] args) {
+        List<String> phrase = new ArrayList<>();
+        List<String> logPhrase = new ArrayList<>();
         try (BufferedReader in = new BufferedReader(new FileReader(".\\chapter_002\\data\\questions.txt"));
              PrintWriter log = new PrintWriter(new FileWriter(".\\chapter_002\\data\\logBot.txt"))) {
-            String[] inputQuestion = in.readLine().split(",");
+            phrase = Arrays.asList(in.readLine().split(","));
             Scanner scanner = new Scanner(System.in);
             String welcom = "Hi, i'm happy Bot, talk with me";
-            System.out.println(welcom);
-            log.println(welcom + System.lineSeparator());
+            getPrintln(welcom);
+            isAdd(logPhrase, welcom);
             for (int i = 0; i < 2;) {
                 String answer = scanner.nextLine();
                 if (answer.equals("Stop")) {
-                    log.println(answer + System.lineSeparator());
+                    isAdd(logPhrase, answer);
                 } else if (answer.equals("End")) {
-                    log.println(answer + System.lineSeparator());
+                    isAdd(logPhrase, answer);
                     break;
                 } else {
-                    log.println(answer + System.lineSeparator());
-                    String question = inputQuestion[(int) (Math.random() * inputQuestion.length)];
-                    log.println(question + System.lineSeparator());
-                    System.out.println(question);
+                    isAdd(logPhrase, answer);
+                    int index = (int) (Math.random() * logPhrase.size());
+                    String question = phrase.get(index);
+                    isAdd(logPhrase, question);
+                    getPrintln(question);
                 }
-
+            }
+            for (String logPrase : logPhrase
+            ) {
+                log.println(logPrase);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static void getPrintln(String str) {
+        System.out.println(str);
+    }
+
+    private static boolean isAdd(List<String> logPhrase, String answer) {
+        return logPhrase.add(answer + System.lineSeparator());
     }
 }
