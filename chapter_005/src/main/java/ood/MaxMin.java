e30357c04493981e8f4b83815ac0ab90c59e37b0
fagot2005@gmail.com <v.lashchenko.1976@gmail.com>
2020-08-25 23:07:30 +0300

    1. Принципы Kiss, Dry и Yagni [#313834]

---
+++ chapter_005/src/main/java/ood/MaxMin.java
@@ -0,0 +1,41 @@
+package ood;
+
+import java.util.*;
+
+
+public class MaxMin {
+    public static <T> T max(List<T> value, Comparator<T> comparator) {
+        return value.isEmpty() ? null : maxMin(value, comparator.reversed());
+    }
+
+    public static <T> T min(List<T> value, Comparator<T> comparator) {
+        return value.isEmpty() ? null : maxMin(value, comparator);
+    }
+
+    private static <T> T maxMin(List<T> list, Comparator<T> comparator) {
+        Iterator <T> i = list.iterator();
+        T val = i.next();
+        while (i.hasNext()) {
+            T cur = i.next();
+            if (comparator.compare(val, cur) > 0) {
+                val = cur;
+            }
+        }
+        return val;
+    }
+
+    public static void main(String[] args) {
+                List<Integer> list = new ArrayList<>();
+        list.add(20);
+        list.add(35);
+        list.add(23);
+        list.add(4);
+        list.add(6);
+        list.add(30);
+        max(list, (x, y) -> x.compareTo(y));
+        System.out.println(list);
+        max(list, (x, y) -> y.compareTo(x));
+        System.out.println(list);
+
+    }
+}
